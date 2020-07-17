8e466d004dc0dbbec6e7bb052dda9d253b9a6a56
ernesto.sjunior <ernesto.sjunior@hotmail.com>
2020-07-16 23:07:16 -0300

    Update

---
+++ api.js
@@ -0,0 +1,12 @@
+const url_padrao = 'http://127.0.0.1:8000',
+
+
+function exibirdebts() {
+
+    fetch(url_padrao, {
+            method: 'get',
+            headers: new headers(),
+            body: null
+
+        })
+}
